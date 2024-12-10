bd0a3f2ff7f49f22466b4545505f8913d1db68a5
Nilay Shah <nilayshah630@gmail.com>
2024-12-10 14:30:07 +0530

    application file

--- src/main/java/com/GuessNumberGameApplication.java
+++ src/main/java/com/GuessNumberGameApplication.java
@@ -2,20 +2,26 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class GuessNumberGameApplication {
+public class GuessNumberGameApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuessNumberGameApplication.class, args);
 	}
 
-	
 	@Bean
 	public PasswordEncoder getEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(GuessNumberGameApplication.class);
 	}
 }
